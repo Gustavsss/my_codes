3ca7c351c6c325221c8994b53604305df0a7f77f
Gustavsss <114224491+Gustavsss@users.noreply.github.com>
2022-09-23 14:18:30 +0300

    Create upgraded_game.py

---
+++ upgraded_game.py
@@ -0,0 +1,39 @@
+import random
+import datetime
+
+
+computer_number = random.randint(1, 100)
+continue_game = True
+user_guesses = []
+start_time = datetime.datetime.now()
+
+
+def calculate_average_difference():
+    sum_of_differences = 0
+    for n in user_guesses:
+        sum_of_differences += abs(n - computer_number)
+
+    return sum_of_differences/len(user_guesses)
+
+
+while(continue_game):
+    user_guess = int(input('Uzmini skaitli starp 1 un 100: '))
+    user_guesses.append(user_guess)
+
+    if user_guess == computer_number:
+        print('Precīzi, tu uzminēji!')
+        continue_game = False
+    elif user_guess < computer_number:
+        print('Vairāk')
+    elif user_guess > computer_number:
+        print('Mazāk')
+    else:
+        print('Notika kļūda!') 
+
+print('Game over!')
+
+print(f'Vidējā vērtība bija {calculate_average_difference()}')
+
+
+
+print(f'Tu spēlēji {datetime.datetime.now()-start_time} sekundes')
